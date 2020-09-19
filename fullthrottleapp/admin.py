1fefc1ca40ecc6ea932d659642d9654c6400ed12
vishalhande <61544948+vishalhande@users.noreply.github.com>
2020-09-19 20:12:53 +0530

    fourth commit

--- fullthrottleapp/admin.py
+++ fullthrottleapp/admin.py
@@ -5,6 +5,7 @@ from .models import Members,ActivityPeriods
 # model is registered
 # second commit
 #third commit
+#fourth commit
 admin.site.register(Members)
 admin.site.register(ActivityPeriods)
 
